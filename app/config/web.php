28aa72235b32981cb8114688ea877deb859aa6e6
danilshayakhmetov <abraham_lincoln5@rambler.ru>
2025-09-07 18:50:24 +0500

    обновлены рулзы для админа

--- app/config/web.php
+++ app/config/web.php
@@ -69,6 +69,7 @@ $config = [
                     'pluralize' => false,
                 ],
 
+
                 // CRUD для OrderController
                 'order/<id:\d+>/view'   => 'order/view',
                 'order/<id:\d+>/update' => 'order/update',
